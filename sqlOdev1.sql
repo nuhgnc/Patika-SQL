08e677bf327065935951162a8a97aba7d07f9c6f
nuhgnc <66550954+nuhgnc@users.noreply.github.com>
2022-03-23 15:14:45 +0300

    sql dosyası

--- sqlOdev1.sql
+++ sqlOdev1.sql
@@ -1,5 +1,4 @@
---SELECT title, description  FROM film -- 1.soru
---SELECT * FROM film WHERE length > 60 AND length < 75 -- 2.Soru
---SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99) -- 3. Soru 
---SELECT * FROM customer WHERE first_name = 'Mary' -- 4. Sorunun cevabı = Smith --4. Soru
---SELECT * FROM film WHERE NOT length > 50 AND NOT ( rental_rate = 2.99 OR  rental_rate = 4.99) --5.soru
+--SELECT country FROM country WHERE country LIKE 'A%a'
+--SELECT country FROM country WHERE country LIKE '_____%n'
+--SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
+--SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
